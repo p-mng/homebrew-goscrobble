c533de88e5838e2854b7aa9b46251890385550ac
Patrick Mang <65464529+p-mng@users.noreply.github.com>
2026-08-20 13:37:30 +0200

    Update formula description and dependencies

--- Formula/goscrobble.rb
+++ Formula/goscrobble.rb
@@ -1,5 +1,5 @@
 class Goscrobble < Formula
-  desc "A simple, cross-platform music scrobbler daemon."
+  desc "Simple, cross-platform music scrobbler daemon"
   homepage "https://github.com/p-mng/goscrobble"
   url "https://github.com/p-mng/goscrobble/archive/refs/tags/v0.8.0.tar.gz"
   sha256 "91c16c5737e2cea5a82b9502506409a77297b05189c3824867d458e1de213553"
@@ -7,8 +7,10 @@ class Goscrobble < Formula
 
   depends_on "go" => :build
 
-  depends_on "media-control"
-  depends_on "terminal-notifier"
+  on macos_do
+    depends_on "media-control"
+    depends_on "terminal-notifier"
+  end
 
   def install
     system "go", "build", *std_go_args
